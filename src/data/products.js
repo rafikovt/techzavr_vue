47b5f76006023d6a27a859dfe63c872a5b0564d2
Рафиков Тимур Ильдарович <trafikov@rtrn.ru>
2020-12-26 09:37:26 +0500

    Добавляет проект. Добавляет отрисовку списка товаров в цикле

---
+++ src/data/products.js
@@ -0,0 +1,53 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3960,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 24690,
+    image: '/img/board.jpg',
+  },
+  {
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: 39990,
+    image: '/img/bicycle.jpg',
+  },
+  {
+    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    price: 16560,
+    image: '/img/airpods.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: 8960,
+    image: '/img/phone-3.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: '/img/toothbrush.jpg',
+  },
+  {
+    title: 'Детский трюковой самокат Razor Grom',
+    price: 4990,
+    image: '/img/scooter.jpg',
+  },
+  {
+    title: 'Наушники Sony',
+    price: 30690,
+    image: '/img/headphones.jpg',
+  },
+  {
+    title: 'Гироскутер Razor Hovertrax 2.0',
+    price: 34900,
+    image: '/img/wheels.jpg',
+  },
+  {
+    title: 'Сотовый телефон',
+    price: 21790,
+    image: '/img/phone.jpg',
+  },
+
+];
